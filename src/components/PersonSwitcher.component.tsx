1eea99bdff309277f2d9c1d8ea8fed5417d3adad
Mohamed-Dhia <56113399+mohamed-Dhia@users.noreply.github.com>
2021-02-21 16:31:31 +0100

    (fix) person switcher reads pathname from with router props instead of window.location

--- src/components/PersonSwitcher.component.tsx
+++ src/components/PersonSwitcher.component.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import type ChatStore from '../store/chat';
 
-const PersonSwitcher: FC<{ chatStore: ChatStore }> = ({ chatStore }) => {
+const PersonSwitcher: FC<{ chatStore: ChatStore } & RouteComponentProps> = ({
+  chatStore,
+  location: { pathname },
+}) => {
   const [{ newDataCount: chatState }, setChatState] = useState(
     chatStore.chatState,
   );
-  const location = window.location.href.split('/')[3];
 
   useEffect(() => {
     chatStore.subscribe(setChatState);
@@ -22,19 +24,19 @@ const PersonSwitcher: FC<{ chatStore: ChatStore }> = ({ chatStore }) => {
       <Link to="/first-person">
         <button type="button" className="switcher">
           Person1
-          {location !== 'first-person' &&
-            location.length > 1 &&
+          {pathname !== '/first-person' &&
+            pathname.length > 1 &&
             messageNotification}
         </button>
       </Link>
       <Link to="/second-person">
         <button type="button" className="switcher">
           Person2
-          {location !== 'second-person' && messageNotification}
+          {pathname !== '/second-person' && messageNotification}
         </button>
       </Link>
     </div>
   );
 };
 
-export default PersonSwitcher;
+export default withRouter(PersonSwitcher);
